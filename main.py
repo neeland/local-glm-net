4bc8c1de0a7c68a043e01a67c3909322005c5ef3
neelan pather <neelan@analytixengine.com>
2023-10-15 18:38:58 +0000

    15 Oct 2023, 20:38

--- main.py
+++ main.py
@@ -166,13 +166,15 @@ def train_model(X, v, y, device):
     return localglmnet
 
 def get_device():
-        """
-        Returns the device to use for training.
+    """
+    Returns the device to use for training.
 
-        Returns:
-        torch.device: Device to use for training.
-        """
-        return torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+    Returns:
+    torch.device: Device to use for training.
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+    print(f"torch device: {device}")
+    return device
 
 # writing main function
 
